b075ee347c8017891dd179fd2a768977c9eea46f
Eray <37340907+erayack@users.noreply.github.com>
2024-03-01 23:27:11 +0300

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ import { expressiveCodeOptions } from "./src/site.config";
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://erayack.github.io/",
+	site: "https://erayack.github.io/prod",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
 		rehypePlugins: [
