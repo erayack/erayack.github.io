80fc918641e3433626c2648eb64fb27b89a1392d
Eray <37340907+erayack@users.noreply.github.com>
2024-03-03 15:52:49 +0300

    update

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,7 +5,7 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Eray Ack",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Eray Ack Blog",
+	title: "Eray Ack",
 	// Meta property used as the default description meta property
 	description: "Personal blog of Eray Ack",
 	// HTML lang property, found in src/layouts/Base.astro L:18
